631b0e580376c756fc72a1470a0d6c42a80a7d2a
준석 손 <thstjr0487@naver.com>
2024-11-13 03:03:21 +0900

    11111

--- CustomControls/Controls/KoroArmCC.cs
+++ CustomControls/Controls/KoroArmCC.cs
@@ -5,6 +5,17 @@ namespace CustomControls.Controls
 {
     public class KoroArmCC : Control
     {
+
+
+        public static readonly DependencyProperty IsRotateProperty = DependencyProperty.Register("IsRotate", typeof(bool), typeof(KoroArmCC), new PropertyMetadata(false));
+        public bool IsRotate
+        {
+            get { return (bool)GetValue(IsRotateProperty); }
+            set { SetValue(IsRotateProperty, value); }
+        }
+
+
+
         static KoroArmCC()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(KoroArmCC), new FrameworkPropertyMetadata(typeof(KoroArmCC)));
